ace26d6c9f4ec9b4245a3d436b2e6a018a5bc1ef
vincentvg <vincent.gelinas.3@ulaval.ca>
2025-09-23 09:40:04 -0400

    Fix it omg2

--- R/main.R
+++ R/main.R
@@ -563,20 +563,21 @@ diagnosis <- function(analysis, save_path = ".", file_name = "analysis", organis
   coordinated <- length(assay_presence)
   combined_frame[["section"]][1:length(assay_presence)] <- "Assay"
   print(combined_frame)
-  print(coordinated)
   coordinated <- coordinated + 1
+  print(coordinated)
   combined_frame[["section"]][coordinated] <- "Normalization"
   print(combined_frame)
-  print(coordinated)
   coordinated <- coordinated + 1
+  print(coordinated)
   combined_frame[["section"]][coordinated] <- "Scaling"
   print(combined_frame)
-  print(coordinated)
+  
   coordinated <- coordinated + 1
+  print(coordinated)
   combined_frame[["section"]][coordinated:(coordinated + 2)] <- "Reductions"
   print(combined_frame)
-  print(coordinated)
   coordinated <- coordinated + 3
+  print(coordinated)
   combined_frame[["section"]][coordinated:(coordinated + length(metadata_list))] <- "MetaData"
 
   # Make the diagnosis report
